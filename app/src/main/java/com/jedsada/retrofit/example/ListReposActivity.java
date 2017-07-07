4a282c11217b5481ab0a2e10e6a889266deb0ad5
Jedsada Tiwongvorakul <jt@20scoops.com>
2017-07-07 09:54:20 +0700

    mock data to show list.

--- app/src/main/java/com/jedsada/retrofit/example/ListReposActivity.java
+++ app/src/main/java/com/jedsada/retrofit/example/ListReposActivity.java
@@ -1,18 +1,17 @@
 package com.jedsada.retrofit.example;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.jedsada.retrofit.example.adapter.RepoAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class ListReposActivity extends AppCompatActivity {
 
@@ -20,6 +19,7 @@ public class ListReposActivity extends AppCompatActivity {
     private RecyclerView list;
     private RepoAdapter adapterListRepos;
 
+    @SuppressLint("DefaultLocale")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,20 +30,36 @@ public class ListReposActivity extends AppCompatActivity {
         list.setAdapter(adapterListRepos);
         setupProgressDialog();
         progressDialog.show();
-        NetworkModule.getInstance().githubService().getUserRepos("pondthaitay")
-                .enqueue(new Callback<List<UserReposDao>>() {
-                    @Override
-                    public void onResponse(Call<List<UserReposDao>> call, Response<List<UserReposDao>> response) {
-                        progressDialog.dismiss();
-                        adapterListRepos.setData(response.body());
-                    }
 
-                    @Override
-                    public void onFailure(Call<List<UserReposDao>> call, Throwable t) {
-                        // TODO : handler error
-                        progressDialog.dismiss();
-                    }
-                });
+        final List<UserReposDao> list = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            UserReposDao userReposDao = new UserReposDao();
+            userReposDao.setName(String.format("Loop : %d", i));
+            userReposDao.setLanguage(String.valueOf(i));
+            list.add(userReposDao);
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                progressDialog.dismiss();
+                adapterListRepos.setData(list);
+            }
+        }, 1000);
+//        NetworkModule.getInstance().githubService().getUserRepos("pondthaitay")
+//                .enqueue(new Callback<List<UserReposDao>>() {
+//                    @Override
+//                    public void onResponse(Call<List<UserReposDao>> call, Response<List<UserReposDao>> response) {
+//
+//                        adapterListRepos.setData(response.body());
+//                    }
+//
+//                    @Override
+//                    public void onFailure(Call<List<UserReposDao>> call, Throwable t) {
+//                        // TODO : handler error
+//                        progressDialog.dismiss();
+//                    }
+//                });
     }
 
     private void setupProgressDialog() {
